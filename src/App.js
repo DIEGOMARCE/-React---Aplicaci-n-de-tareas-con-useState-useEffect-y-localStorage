2de83a330d97277faab58cd376576e33bf54da1e
Diego Sanhueza <diegosanhueza@hotmail.es>
2022-07-21 23:14:34 -0400

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { TaskCreator } from './components/TaskCreator'
+import { TaskTable } from './components/TaskTable'
+import { VisibilityControl } from './components/VisibilityControl';
+import {Container} from './components/Container'
+
 
 function App() {
+
+
+  const [tasksItems, setTasksItems] = useState([]);
+  const [showCompleted, setshowCompleted] = useState(false)
+
+  function createNewTask(tasksName) {
+
+
+    if (!tasksItems.find(tasks => tasks.name === tasksName)) {
+      setTasksItems([...tasksItems, { name: tasksName, done: false }])
+    }
+
+
+  }
+
+  const toggleTask = (tasks) => {
+    setTasksItems(
+
+      tasksItems.map((t) => (t.name === tasks.name) ? { ...t, done: !t.done } : t)
+    );
+  };
+
+
+  useEffect(() => {
+    let data = localStorage.getItem('tasks')
+    if (data) {
+      setTasksItems(JSON.parse(data))
+    }
+  }, [])
+
+
+  const cleanTasks = () => {
+    setTasksItems(tasksItems.filter(tasks => !tasks.done))
+    setshowCompleted(false)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasksItems))
+
+  }, [tasksItems])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-dark vh-100 text-white">
+<Container>
+<TaskCreator createNewTask={createNewTask} />
+      <TaskTable tasks={tasksItems} toggleTask={toggleTask} />
+
+      <VisibilityControl
+        isChecked={showCompleted}
+        setshowCompleted={(checked) => setshowCompleted(checked)}
+        cleanTasks={cleanTasks}
+
+      />
+
+      {
+        showCompleted === true && (
+          <TaskTable tasks={tasksItems} toggleTask={toggleTask} showCompleted={showCompleted} 
+          />
+        )
+      }
+
+
+</Container>
+
+      </main>
   );
 }
 
---
+++ src/components/taskRow.js
@@ -0,0 +1,17 @@
+export const TaskRow = ({ tasks, toggleTask }) => {
+    return (
+        <tr >
+            <td className="d-flex justify-content-between">
+                {tasks.name}
+                <input type="checkbox"
+                    checked={tasks.done}
+                    onChange={() => toggleTask(tasks)}
+                />
+            </td>
+        </tr>
+
+
+
+    )
+
+}
